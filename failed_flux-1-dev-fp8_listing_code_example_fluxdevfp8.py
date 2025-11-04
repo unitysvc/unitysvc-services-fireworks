501538081a9c8f6b543ba309d5038a930bc22d92
ronak-hiwale-unitysvc <ronakhiwale@unitysvc.com>
2025-11-03 20:04:49 -0600

    All Working Code Fireworks

---
+++ failed_flux-1-dev-fp8_listing_code_example_fluxdevfp8.py
@@ -0,0 +1,31 @@
+# Environment variables used for this test:
+# API_KEY=fw_3Zi68xUugi1ZrFnAn31LW4BY
+# API_ENDPOINT=https://api.fireworks.ai/inference/v1
+#
+# To reproduce this test, export these variables:
+# export API_KEY='fw_3Zi68xUugi1ZrFnAn31LW4BY'
+# export API_ENDPOINT='https://api.fireworks.ai/inference/v1'
+#
+
+from openai import OpenAI
+import os
+
+# Use environment variables or fallback
+#os.environ.get("API_ENDPOINT") or 
+#os.environ.get("API_KEY")
+base_url = os.environ.get("API_ENDPOINT")
+api_key = os.environ.get("API_KEY")
+
+client = OpenAI(api_key=api_key, base_url=base_url)
+
+response = client.chat.completions.create(
+    model= "accounts/fireworks/deployedModels/flux-1-dev-fp8",
+
+
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Say this is a test"}
+    ]
+)
+
+print(response.choices[0].message.content)
